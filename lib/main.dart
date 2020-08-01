37c148398a8f2a214bdda706b40e396033e30246
kaliprasad1998 <kancharlaguruprasad@gmail.com>
2020-08-01 10:23:20 +0530

    changes till august 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
-  );
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage('images/prasad.jpg'),
+          ),
+          Text('Prasad Kancharla',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                color: Colors.white,
+                fontSize: 40.0,
+              )),
+          Text(
+            'FULL STACK JAVA DEVELOPER',
+            style: TextStyle(
+                fontFamily: 'SourceSansPro',
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.5,
+                fontSize: 20.0,
+                color: Colors.teal[100]),
+          )
+        ],
+      )),
+    ));
+  }
 }
